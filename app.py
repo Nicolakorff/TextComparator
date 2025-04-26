0a67d74bb999ee5779ab36d2ef29a315134c866c
Nicola Korff <nicolavonkorff@gmail.com>
2025-04-26 23:29:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import importlib.util
 
 if importlib.util.find_spec("en_core_web_sm") is None:
     subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+if importlib.util.find_spec("es_core_news_sm") is None:
+    subprocess.run(["python", "-m", "spacy", "download", "es_core_news_sm"])
 
 try:
     nlp_es = spacy.load("es_core_news_sm")
